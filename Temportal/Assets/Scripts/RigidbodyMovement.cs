e2cad521f517ff42bba9f7c7efc95e22062b3268
Kannen Joshi <22418234+InfernoKay@users.noreply.github.com>
2021-11-23 02:06:13 +0000

    STARTED RB Movement & TODOs
    + Links to videos to help
      ~ Some are not same system / too complex
    
    + Values for speeds can be accessed from Unity
      // Do nothing though
    
    + Movement
      // DOES NOT WORK WELL NEEDS FIXING
      // Moves v. slowly but does accelerate
      // Friction and drag
    
    ---
    
    TODO Follow Camera Script
    TODO Mouse move Script
    TODO Movement to work

--- Temportal/Assets/Scripts/RigidbodyMovement.cs
+++ Temportal/Assets/Scripts/RigidbodyMovement.cs
@@ -1,38 +1,72 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RigidbodyMovement : MonoBehaviour
 {
-    // https://www.youtube.com/watch?v=b1uoLBp2I1w
+    // https://www.youtube.com/watch?v=b1uoLBp2I1w (How to Create Player Movement in UNITY (Rigidbody & Character Controller)
+    // https://www.youtube.com/watch?v=AwJbOJMFfAg (How To: Make a Rigidbody Movement Controller in Unity3D Part 1)
+    // https://www.youtube.com/watch?v=LqnPeqoJRFY (Rigidbody FPS Controller Tutorial #1 | Basic Movement in less than 4 min)
+
+    // Unity Input System HOW TO
+    // https://www.youtube.com/watch?v=5tOOstXaIKE (Controlling Cross-Platform Characters with Unity Input System | Tutorial)
+
+    // https://www.youtube.com/watch?v=bwFLLnhm4D4 (Unity 2020 | Rigidbody FPS Movement Tutorial | Pt. 1)
 
     [Header("Player Components")] 
     [SerializeField] private Transform Camera;
     [SerializeField] private Rigidbody Body;
+
     [Space(5)]
     [Header("Player Variables")]
-    [SerializeField] private float speed = 6;
-    [SerializeField] private float sensitivity = 1; // Mouse sens
-    [SerializeField] private float jumpForce = 15;
+    [SerializeField] private float walkSpeed = 6f;
+    //[SerializeField] private float maxWalkSpeed = 6f;
+
+    [SerializeField] private float sprintSpeed = 8f;
+    //[SerializeField] private float maxSprintSpeed = 8f;
+
+    [SerializeField] private float crouchSpeed = 4f;
+    //[SerializeField] private float maxCrouchSpeed = 4f;
+
+    [SerializeField] private float sensitivity = 1f; // Mouse sens
+    [SerializeField] private float jumpForce = 15f;
+
 
     private Vector3 playerMovementInput;
-    private Vector3 playerMouseInput;
+    private Vector2 playerMouseInput;
+
+    private Vector3 moveDirection;
 
 
     void Update()
     {
-        
+        //playerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        //playerMovementInput = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
     }
 
-    public void Move()
+    private void FixedUpdate()
     {
-        
+        UpdateMovement();
     }
 
-    public void Look()
+
+    private void UpdateMovement()
+    {
+        moveDirection = transform.TransformDirection(playerMovementInput);
+        Body.AddForce(playerMovementInput * walkSpeed, ForceMode.Acceleration);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 inputMovement = context.ReadValue<Vector2>();
+        playerMovementInput = new Vector3(inputMovement.x, 0f, inputMovement.y);
+    }
+
+    public void Look(InputAction.CallbackContext context)
     {
 
     }
 
-    public void Jump()
+    public void Jump(InputAction.CallbackContext context)
     {
 
     }
