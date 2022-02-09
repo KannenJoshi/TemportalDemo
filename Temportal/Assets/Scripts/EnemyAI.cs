315e636ef144b020397094682bef2a40d431ebd9
Kannen Joshi <22418234+InfernoKay@users.noreply.github.com>
2022-02-09 01:22:06 +0000

    STARTED Enemy AI
    // Created File

---
+++ Temportal/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Detect Player
+    }
+
+    private void Fire()
+    {
+        
+    }
+
+    private void Warp()
+    {
+        // Disappear 4D Shift Shader
+        gameObject.SetActive(false);
+    }
+
+    private void CheckWarpEnd()
+    {
+        
+    }
+    
+    
+}
