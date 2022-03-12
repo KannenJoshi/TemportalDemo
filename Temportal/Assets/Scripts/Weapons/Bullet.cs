c56f095e1e19f1fd1cea71316af74f817ec9f05a
Kannen Joshi <kannen.joshi@gmail.com>
2022-03-12 18:11:05 +0000

    STARTED Bullets Through Portal
    Bullet struggling to go through portal, I think because of it being destroyed on collision. Must stop collision with wall behind for all objects
    
    ~ BULLET
    // Disable bullet trail when teleport or will draw a line between portals (TODO doesnt work properly)
    // Check collision to portal wall, if not destroy. Needed or bullet destroys after passes through portal
    // Refactor tag to parentTag to make more sense

--- Temportal/Assets/Scripts/Weapons/Bullet.cs
+++ Temportal/Assets/Scripts/Weapons/Bullet.cs
@@ -10,14 +10,17 @@ public class Bullet : MonoBehaviour
     [SerializeField] private Color enemyColour;// = new Color(255, 103, 16);
 
     private int damage = 10;
-    private string tag;
+    private string _parentTag;
     private TrailRenderer tr;
-
+    private PortalTraveller pt;
+    
+    // So Doesn't hit entity shooting when created
     private bool _ignoreShooterTag = true;
 
     void Awake()
     {
         tr = GetComponent<TrailRenderer>();
+        pt = GetComponent<PortalTraveller>();
     }
 
     void Start()
@@ -27,24 +30,35 @@ public class Bullet : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        // TODO: Edit ignore tag to ignore at first shoot if tag is same, not just for player so Enemy can use
-        if (_ignoreShooterTag && collision.gameObject.tag.Equals(tag)) return;
+        // TODO: Edit ignore _parentTag to ignore at first shoot if _parentTag is same, not just for player so Enemy can use
+        if (_ignoreShooterTag && collision.gameObject.tag.Equals(_parentTag)) return;
+        
         if (collision.gameObject.tag.Equals("Player") || collision.gameObject.tag.Equals("Enemy"))
         {
             collision.gameObject.GetComponent<Entity>().ApplyDamage(damage);
+            Destroy(gameObject);
         }
-
-        if (collision.gameObject.tag.Equals("Portal")) return;
-        Destroy(gameObject);
+        
+        //print(collision.gameObject.tag);
+        if (collision.gameObject.tag.Equals("PortalWall"))
+        {
+            //print("Bullet hit " + collision.gameObject.tag);
+            StartCoroutine(DisableTrailOnTeleport());
+            //Physics.IgnoreCollision(GetComponent<CapsuleCollider>(), collision.collider);
+            //var portal = collision.gameObject.GetComponent<Portal>();
+            //pt.Teleport(portal.transform, portal.OtherPortal.transform);
+        }
+        else
+            Destroy(gameObject);
 
     }
 
     public void SetStats(int damage, string tag)
     {
         this.damage = Mathf.RoundToInt(damage * damageMultiplier);
-        this.tag = tag;
+        this._parentTag = tag;
 
-        //tr.material.color = tag.Equals("Player") ? playerColour : enemyColour;
+        //tr.material.color = _parentTag.Equals("Player") ? playerColour : enemyColour;
         tr.material.SetColor("_Base", tag.Equals("Player") ? playerColour : enemyColour);
     }
 
@@ -52,5 +66,12 @@ public class Bullet : MonoBehaviour
     {
         yield return new WaitForSeconds(0.1f);
         _ignoreShooterTag = false;
+    }
+
+    IEnumerator DisableTrailOnTeleport()
+    {
+        tr.enabled = false;
+        yield return new WaitForEndOfFrame();
+        tr.enabled = true;
     }
 }
