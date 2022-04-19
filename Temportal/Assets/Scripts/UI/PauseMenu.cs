3f9a4607f9e39433dbe437d08048f0b8eadda62f
Kannen Joshi <kannen.joshi@gmail.com>
2022-04-19 14:53:43 +0100

    Pause Menu

---
+++ Temportal/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,78 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class PauseMenu : MonoBehaviour
+{
+    private static bool _isPaused;
+
+    public static bool IsPaused
+    {
+        get => _isPaused;
+        set
+        {
+            _isPaused = value;
+            if (value)
+                Pause();
+            else
+            {
+                UnPause();
+            }
+        }
+    }
+
+    private static GameObject _pauseScreen;
+    
+    [SerializeField] private GameObject options;
+
+    private static float _oldTimeScale;
+    
+    private void Awake()
+    {
+        _pauseScreen = transform.GetChild(0).gameObject;
+    }
+
+    public void ResumeGame()
+    {
+        UnPause();
+    }
+
+    public void OpenOptions()
+    {
+        options.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        options.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("QUITTING...");
+    }
+
+    private static void Pause()
+    {
+        _isPaused = true;
+        _oldTimeScale = Time.timeScale;
+        Time.timeScale = 0;
+        
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        
+        _pauseScreen.SetActive(true);
+    }
+
+    private static void UnPause()
+    {
+        _isPaused = false;
+        Time.timeScale = _oldTimeScale;
+                
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
+        _pauseScreen.SetActive(false);
+    }
+}
